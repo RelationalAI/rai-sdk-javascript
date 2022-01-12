9ef85ac50ffdf387e26c03b246557819972cd692
Denis Gursky <denis.gursky@relational.ai>
2022-01-12 16:41:33 -0500

    green build

--- index.ts
+++ index.ts
@@ -12,5 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-export { boo } from './src/config';
+export { readConfig } from './src/config';
 export * from './src/foo';
