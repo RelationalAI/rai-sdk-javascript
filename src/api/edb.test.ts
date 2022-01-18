82b00dc0fa27cab0817b8e4da05d0c863629e912
Denis Gursky <denis.gursky@relational.ai>
2022-01-18 11:52:50 -0500

    added edbs

---
+++ src/api/edb.test.ts
@@ -0,0 +1,75 @@
+import nock from 'nock';
+
+import * as endpoint from './edb';
+import { getMockContext, nockTransaction } from './testUtils';
+import { RelKey } from './transaction';
+
+describe('edb', () => {
+  const context = getMockContext();
+  const mockEdbs: RelKey[] = [
+    {
+      values: ['RAI_VariableSizeStrings.VariableSizeString'],
+      name: 'edb1',
+      keys: [':region', 'RelationalAITypes.FilePos'],
+      type: 'RelKey',
+    },
+    {
+      values: ['RAI_VariableSizeStrings.VariableSizeString'],
+      name: 'edb2',
+      keys: [':region', 'RelationalAITypes.FilePos'],
+      type: 'RelKey',
+    },
+  ];
+  const database = 'test-db';
+  const engine = 'test-engine';
+
+  afterEach(() => nock.cleanAll());
+  afterAll(() => nock.restore());
+
+  it('should list edbs', async () => {
+    const scope = nockTransaction(
+      [
+        {
+          type: 'ListEdbAction',
+        },
+      ],
+      [
+        {
+          type: 'ListEdbActionResult',
+          rels: mockEdbs,
+        },
+      ],
+      database,
+      engine,
+    );
+    const result = await endpoint.listEdbs(context, database, engine);
+
+    scope.done();
+
+    expect(result).toEqual(mockEdbs);
+  });
+
+  it('should delete edb', async () => {
+    const scope = nockTransaction(
+      [
+        {
+          type: 'ModifyWorkspaceAction',
+          delete_edb: 'edb1',
+        },
+      ],
+      [
+        {
+          type: 'ModifyWorkspaceActionResult',
+          delete_edb_result: [mockEdbs[1]],
+        },
+      ],
+      database,
+      engine,
+    );
+    const result = await endpoint.deleteEdb(context, 'edb1', database, engine);
+
+    scope.done();
+
+    expect(result).toEqual([mockEdbs[1]]);
+  });
+});
---
+++ src/api/edb.ts
@@ -0,0 +1,56 @@
+// Copyright 2022 RelationalAI, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the 'License');
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { Context } from './context';
+import {
+  ListEdbAction,
+  ModifyWorkspaceAction,
+  runActions,
+} from './transaction';
+
+export async function listEdbs(
+  context: Context,
+  database: string,
+  engine?: string,
+) {
+  const action: ListEdbAction = {
+    type: 'ListEdbAction',
+  };
+  const result = await runActions(context, [action], database, engine);
+
+  if (result.actions[0]?.result?.type === 'ListEdbActionResult') {
+    return result.actions[0].result.rels;
+  }
+
+  throw new Error('ListEdbActionResult is missing');
+}
+
+export async function deleteEdb(
+  context: Context,
+  name: string,
+  database: string,
+  engine?: string,
+) {
+  const action: ModifyWorkspaceAction = {
+    type: 'ModifyWorkspaceAction',
+    delete_edb: name,
+  };
+  const result = await runActions(context, [action], database, engine);
+
+  if (result.actions[0]?.result?.type === 'ModifyWorkspaceActionResult') {
+    return result.actions[0].result.delete_edb_result;
+  }
+
+  throw new Error('ModifyWorkspaceActionResult is missing');
+}
