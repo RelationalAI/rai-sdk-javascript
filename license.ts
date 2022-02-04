8c81313365c15d0999de2d496250e7581db609d5
Denis Gursky <denis.gursky@relational.ai>
2022-02-04 14:34:52 -0500

    added license headers and eslint plugin to check license headers

--- license.ts
+++ license.ts
@@ -13,21 +13,3 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-
-export type User = {
-  id: string;
-  email: string;
-  roles: UserRole[];
-  id_providers: string[];
-  status: UserStatus | '';
-};
-
-export enum UserStatus {
-  ACTIVE = 'ACTIVE',
-  INACTIVE = 'INACTIVE',
-}
-
-export enum UserRole {
-  USER = 'user',
-  ADMIN = 'admin',
-}
