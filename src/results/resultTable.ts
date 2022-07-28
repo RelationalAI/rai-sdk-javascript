7ab54a2caca837aaddd5d9288965f75d78d14f2f
Denis Gursky <denis.gursky@relational.ai>
2022-07-28 14:45:56 -0400

    added jsdoc comments

--- src/results/resultTable.ts
+++ src/results/resultTable.ts
@@ -21,20 +21,61 @@ import { ArrowRelation } from '../api/transaction/types';
 import { convertValue, getDisplayValue, getTypeDef } from './resultUtils';
 import { RelTypeDef, RelTypedValue } from './types';
 
-export interface ResultColumn extends IteratorOf<RelTypedValue['value']> {
+export interface ResultColumn {
+  /**
+   * Returns type definition of the column
+   *
+   * @returns Type definition.
+   */
   typeDef: RelTypeDef;
+
+  /**
+   * Returns an iterator over column values.
+   *
+   * @returns An iterator over column values.
+   */
+  [Symbol.iterator](): IterableIterator<RelTypedValue['value']>;
+
+  /**
+   * Returns column values as an array
+   *
+   * @returns An array of column values.
+   */
+  values: () => RelTypedValue['value'][];
+
+  /**
+   * Number of values in the column.
+   *
+   * @returns Number of values.
+   */
+  readonly length: number;
 }
 
 interface IteratorOf<T> {
   [Symbol.iterator](): IterableIterator<T>;
   values: () => T[];
-  length: number;
+  readonly length: number;
 }
 
+/**
+ * ResultTable provides an interface over {@link ArrowRelation} that maps Rel
+ * types to their corresponding JavaScript equivalents.
+ */
 export class ResultTable implements IteratorOf<RelTypedValue['value'][]> {
   private table: Table;
   private typeDefs: RelTypeDef[];
 
+  /**
+   * Instantiate a new ResultTable instance.
+   *
+   * @example
+   *   cosnt result = await client.exec('database', 'engine', 'def output = 123, "test"')
+   *   cosnt resultTable = new ResultTable(result.results[0]);
+   *
+   *   console.log(resultTable.values()); // Prints [[123n, "test"]];
+   *
+   * @param relation Arrow relation
+   */
   constructor(relation: ArrowRelation) {
     this.table = relation.table;
 
@@ -56,10 +97,20 @@ export class ResultTable implements IteratorOf<RelTypedValue['value'][]> {
     this.typeDefs = columnTypes.map(getTypeDef);
   }
 
+  /**
+   * The number of columns in this table.
+   *
+   * @returns The number of columns.
+   */
   get columnLength() {
     return this.table.numCols;
   }
 
+  /**
+   * Return an array of columns.
+   *
+   * @returns An array of columns.
+   */
   columns() {
     const columns = [];
 
@@ -70,6 +121,12 @@ export class ResultTable implements IteratorOf<RelTypedValue['value'][]> {
     return columns;
   }
 
+  /**
+   * Return column at the given index.
+   *
+   * @param index The column index.
+   * @returns The column, or undefined if the index is out of range.
+   */
   columnAt(index: number) {
     const arrowColumn = this.table.getChildAt(index);
     const typeDef = this.typeDefs[index];
@@ -99,10 +156,20 @@ export class ResultTable implements IteratorOf<RelTypedValue['value'][]> {
     return column;
   }
 
+  /**
+   * The number of rows in this table.
+   *
+   * @returns The number of rows.
+   */
   get length() {
     return this.table.numRows;
   }
 
+  /**
+   * Return an iterator over rows.
+   *
+   * @returns An iterator over rows
+   */
   *[Symbol.iterator]() {
     for (const arrowRow of this.table) {
       const row = arrowRow
@@ -113,10 +180,21 @@ export class ResultTable implements IteratorOf<RelTypedValue['value'][]> {
     }
   }
 
+  /**
+   * Return an array of rows.
+   *
+   * @returns An array of rows.
+   */
   values() {
     return Array.from(this);
   }
 
+  /**
+   * Return row at the given index.
+   *
+   * @param {string} index Row index.
+   * @returns The row, or undefined if the index is out of range.
+   */
   get(index: number) {
     const arrowRow = this.table.get(index);
 
@@ -129,7 +207,15 @@ export class ResultTable implements IteratorOf<RelTypedValue['value'][]> {
     }
   }
 
-  slice(begin?: number | undefined, end?: number | undefined) {
+  /**
+   * Return a sub-section of this Table.
+   *
+   * @param begin The beginning of the specified portion of the Table.
+   * @param end The end of the specified portion of the Table. This is
+   *   exclusive of the element at the index 'end'.
+   * @returns An array of rows.
+   */
+  slice(begin: number | undefined, end?: number | undefined) {
     const slicedTable = this.table.slice(begin, end);
 
     return slicedTable.toArray().map(arrowRow => {
@@ -139,6 +225,11 @@ export class ResultTable implements IteratorOf<RelTypedValue['value'][]> {
     });
   }
 
+  /**
+   * Prints this table using console.log.
+   *
+   * Note: it uses getDisplayValue function to convert values to strings.
+   */
   print() {
     const pTable = new PrintTable({
       columns: this.typeDefs.map((typeDef, i) => ({
