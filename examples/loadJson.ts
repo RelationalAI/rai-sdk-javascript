c33a167d5fee6d7be22a1295ecef75d1d9a8bfbd
helmi.nour <helmiinour@gmail.com>
2022-10-12 17:48:23 +0100

    cleanup

--- examples/loadJson.ts
+++ examples/loadJson.ts
@@ -19,7 +19,7 @@ import { promises } from 'fs';
 import path from 'path';
 
 import { Client, readConfig } from '../index.node';
-import { show } from './show';
+import { showTransactionResult } from './show';
 
 async function run(
   database: string,
@@ -36,7 +36,7 @@ async function run(
   const client = new Client(config);
   const result = await client.loadJson(database, engine, relation, json);
 
-  show(result);
+  showTransactionResult(result);
 }
 
 (async () => {
