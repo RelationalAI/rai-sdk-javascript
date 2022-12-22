d7f12890509ebb840106b7c1aa2e187ea846a0a2
Denis Gursky <denis.gursky@relational.ai>
2022-12-22 09:34:29 -0500

    trying github-actions reporter

--- jest.config.js
+++ jest.config.js
@@ -38,6 +38,7 @@ const config = {
   globalSetup: '<rootDir>/jest.setup.ts',
   globalTeardown: '<rootDir>/jest.teardown.ts',
   testTimeout: 120000,
+  reporters: ['default', 'github-actions'],
 };
 
 export default config;
