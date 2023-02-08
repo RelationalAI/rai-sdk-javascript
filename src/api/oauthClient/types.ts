768109f46bde55efe6a7f91d1bac2f06702b048b
Ilian Aleixev <akrion@gmail.com>
2023-02-08 12:05:45 -0600

    Add missing permission

--- src/api/oauthClient/types.ts
+++ src/api/oauthClient/types.ts
@@ -41,6 +41,7 @@ export enum Permission {
   DELETE_DATABASE = 'delete:database',
   // transactions
   RUN_TRANSACTION = 'run:transaction',
+  RUN_READ_ONLY_TRANSACTION = 'run-read:transaction',
   LIST_TRANSACTION = 'list:transaction',
   DELETE_TRANSACTION = 'delete:transaction',
   READ_TRANSACTION = 'read:transaction',
